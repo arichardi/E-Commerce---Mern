6b7e86a0d764a5221dc008997104025b79cb7811
Andre Richardi <a.richardi@gmail.com>
2021-04-19 10:18:42 -0300

    react-router implement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
 import {Container} from 'react-bootstrap'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-4' >
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/products/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
