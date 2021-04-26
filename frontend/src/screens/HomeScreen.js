ef3d269e4a700bb8de3a0209bdc8f94d10d01f94
Andre Richardi <a.richardi@gmail.com>
2021-04-26 09:53:43 -0300

    redux products WIP 2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Products from '../components/Products'
-import axios from 'axios'
+import listProducts from '../redux/Products/productsAction'
 
 
 const HomeScreen = () => {
-
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector( (state) => state.productList)
+    const { loading, error, products} = productList
 
     useEffect( () => {
-        axios.get('/api/products')
-            .then( response => setProducts(response.data))
-            .catch( err => console.log(err))
-
-    } ,[])
+        dispatch(listProducts())
+    } ,[dispatch])
 
     return (
         <>
          <h1>Latest Arts</h1>
+         { loading ? <h2> Loading ...</h2> : error ? <h3>{error}</h3> : 
          <Row>
              {products.map( (product) => {
                  return (
-                 <Col key={product._id} sm={3} ms={6} lg={4} xl={3} >
+                     <Col key={product._id} sm={3} ms={6} lg={4} xl={3} >
                     <Products product={product} />
                  </Col>
              )})}
          </Row>
+            }
         </>
     )
 }
